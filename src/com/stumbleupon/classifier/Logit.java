54cc00e5931cc5a40d6b78fbd74a7825ebceca2c
harishl <harish_l@live.com>
2013-10-20 15:37:59 +0800

    minor modification for 10 fold cross validation in Logistic regression

--- src/com/stumbleupon/classifier/Logit.java
+++ src/com/stumbleupon/classifier/Logit.java
@@ -18,66 +18,59 @@ public class Logit {
 	private DataSource trainSource;
 	private DataSource testSource;
 	private Logistic logit;
+	private int[] URL_ids;
 	
 	public Logit()  throws Exception {
+		//get data
 		trainSource = new DataSource("data/train_categoryFixed.arff");
 		trainData = trainSource.getDataSet();
-		if (trainData.classIndex() == -1) {
-			trainData.setClassIndex(trainData.numAttributes() - 1);
-		}
-		for(int i = 0; i < trainData.numAttributes(); i++) {
-			trainData.deleteWithMissing(i);
-		}
+		
+		// randomize the data
 		trainData.randomize(trainData.getRandomNumberGenerator(System.nanoTime()));
-		trainData.stratify(10); //10 folds
-		
-		//testSource = new DataSource("data/test_from_train.arff");
-		//testData = testSource.getDataSet();
-		testData = trainData.testCV(10, 5);
-		if (testData.classIndex() == -1) {
-			testData.setClassIndex(testData.numAttributes() - 1);
-		}
-		for(int i = 0; i < testData.numAttributes(); i++) {
-			testData.deleteWithMissing(i);
-		}
-		
 		logit = new Logistic();
 	}
 	
 	public void selectAttributes(int numAttr)  throws Exception {
-		// load data
-		int[] indices = new int[numAttr + 1]; //+1 is for the class attribute
+		int[] indices = new int[numAttr + 2]; //+1 is for the class attribute; another +1 for URLID
 		FeatureSelection ftrSelection = new FeatureSelection();
 		indices = ftrSelection.useLowLevel(trainData, numAttr);
-		int[] toRemove = indices;
+		int[] toRetain = indices;
 		Remove remove = new Remove();
-		remove.setAttributeIndicesArray(toRemove);
 		remove.setInvertSelection(true);
+		remove.setAttributeIndicesArray(toRetain);
 		remove.setInputFormat(trainData);
 		Instances trainNew = Filter.useFilter(trainData, remove);
 		trainData = trainNew;
+		if (trainData.classIndex() == -1) {
+			trainData.setClassIndex(trainData.numAttributes() - 1);
+		}
 		System.out.println(trainData.toSummaryString());
-		Instances testNew = Filter.useFilter(testData, remove);
-		testData = testNew;
-		System.out.println(testData.toSummaryString());
 	}
 	
 	public void train() throws Exception {
 		logit.buildClassifier(trainData);
 	}
 	
-	public void test() throws Exception {
-		List<Double> misClassifiedURLs = new ArrayList<Double>();
+	public void test(int numFolds) throws Exception {
+		int numMisClassified = 0;
+		int totalTested = 0;
 		int classification;
-		for(int i = 0; i < testData.numInstances(); i++) {
-			classification = (int) logit.classifyInstance(testData.instance(i));
-			System.out.println(classification + " | " + (int)testData.instance(i).value(testData.classIndex()));
-			if(classification != (int) testData.instance(i).value(testData.classIndex())) {
-				misClassifiedURLs.add(testData.instance(i).value(0));
+		System.out.println("Prediction" + "\t|\t" + "Actual");
+		for(int n=0; n < numFolds; n++) {
+			testData = trainData.testCV(numFolds, n);
+			totalTested = totalTested + testData.numInstances();
+			for(int i = 0; i < testData.numInstances(); i++) {
+				classification = (int) logit.classifyInstance(testData.instance(i));
+				System.out.println(classification + "\t|\t" + (int)testData.instance(i).value(testData.classIndex()));
+				if(classification != (int) testData.instance(i).value(testData.classIndex())) {
+					numMisClassified = numMisClassified + 1;
+				}
 			}
+		//System.out.println("mis-classified categories: \n" + Utils.arrayToString(misClassifiedURLs.toArray()));
 		}
-		System.out.println("%mis-classification: " + (misClassifiedURLs.size()*100.0/testData.numInstances()));
-		//System.out.println("URL ids: \n" + Utils.arrayToString(misClassifiedURLs.toArray()));
+		System.out.println("numMisClassified: " +  numMisClassified);
+		System.out.println("totalTested: " +  totalTested);
+		System.out.println("%mis-classification: " + (numMisClassified*100.0/totalTested));
 	}
 	
 	public double[] predictClass(Instance instance) throws Exception {
@@ -88,6 +81,6 @@ public class Logit {
 		Logit logistic = new Logit();
 		logistic.selectAttributes(5);
 		logistic.train();
-		logistic.test();
+		logistic.test(10); // num folds
 	}
 }
